987c8bf34959dab91bda0a86a381b4c87c19b0db
Hurr1caneAlpha <nathanmoore20@hotmail.co.uk>
2014-07-03 13:57:26 +0100

    Pushing for Episode 6.

--- src/main/java/com/hurr1cane/letsmodreboot/reference/Reference.java
+++ src/main/java/com/hurr1cane/letsmodreboot/reference/Reference.java
@@ -5,4 +5,6 @@ public class Reference {
     public static final String MOD_ID = "LetsModReboot";
     public static final String MOD_NAME = "Let's Mod Reboot";
     public static final String VERSION = "1.7.2-1.0";
+    public static final String CLIENT_PROXY ="com.hurr1cane.letsmodreboot.proxy.ClientProxy";
+    public static final String SERVER_PROXY="com.hurr1cane.letsmodreboot.proxy.ServerProxy";
 }
