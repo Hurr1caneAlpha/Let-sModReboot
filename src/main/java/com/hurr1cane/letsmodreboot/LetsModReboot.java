bbc5085e5b52e2d05df3e383de25adae4635e96c
Hurr1caneAlpha <nathanmoore20@hotmail.co.uk>
2014-07-05 15:05:59 +0100

    Pushing for Episode 7.5

--- src/main/java/com/hurr1cane/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/hurr1cane/letsmodreboot/LetsModReboot.java
@@ -1,45 +1,44 @@
 package com.hurr1cane.letsmodreboot;
 
-import com.hurr1cane.letsmodreboot.configuration.ConfigurationHandler;
+import com.hurr1cane.letsmodreboot.handler.ConfigurationHandler;
 import com.hurr1cane.letsmodreboot.proxy.IProxy;
 import com.hurr1cane.letsmodreboot.reference.Reference;
+import com.hurr1cane.letsmodreboot.util.LogHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid= Reference.MOD_ID,name= Reference.MOD_NAME,version= Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class LetsModReboot
 {
-
     @Mod.Instance(Reference.MOD_ID)
     public static LetsModReboot instance;
 
-    @SidedProxy(clientSide = Reference.CLIENT_PROXY,serverSide =Reference.SERVER_PROXY)
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        LogHelper.info("Pre Initialization Complete!");
     }
 
     @Mod.EventHandler
-    public void Init(FMLInitializationEvent event)
+    public void init(FMLInitializationEvent event)
     {
-
+        LogHelper.info("Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.info("Post Initialization Complete!");
     }
-
-
-
-
-
-
 }
+
+
+
+
---
+++ src/main/java/com/hurr1cane/letsmodreboot/client/gui/ModGuiConfig.java
@@ -0,0 +1,21 @@
+package com.hurr1cane.letsmodreboot.client.gui;
+
+import com.hurr1cane.letsmodreboot.handler.ConfigurationHandler;
+import com.hurr1cane.letsmodreboot.reference.Reference;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+public class ModGuiConfig extends GuiConfig
+{
+    public ModGuiConfig(GuiScreen guiScreen)
+    {
+        super(guiScreen,
+                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+    }
+}
